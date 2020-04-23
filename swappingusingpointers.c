fa3108ce6e1e04e6cea4a51f7d33b3aa41483fac
shreeharikulkarni19112001 <63485579+shreeharikulkarni19112001@users.noreply.github.com>
2020-04-23 10:54:34 +0530

    Update swappingusingpointers.c

--- swappingusingpointers.c
+++ swappingusingpointers.c
@@ -14,4 +14,16 @@ int main()
 	*pnum1=*pnum1-*pnum2;
 	printf("Value after swapping is num1=%d and num2=%d\n",*pnum1,*pnum2);
 }
-	
+
+OUPUT
+enter two numbers
+5
+7
+Value of num1=5 and num2=7
+Value after swapping is num1=7 and num2=5
+
+
+------------------
+(program exited with code: 0)
+
+Press any key to continue . . .
